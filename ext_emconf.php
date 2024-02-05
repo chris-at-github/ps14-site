44c119c9897450e3fcf9133e867288949c063076
Christian Pschorr <pschorr.christian@gmail.com>
2024-02-05 22:13:21 +0100

    Basis Installation für Sitepackage-Extension

---
+++ ext_emconf.php
@@ -0,0 +1,31 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext "sitepackage".
+ *
+ * Auto generated on 20-08-2020 12:55.
+ *
+ * Manual updates:
+ * Only the data in the array - everything else is removed by next
+ * writing. "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = [
+	'title' => 'Ps14 Site',
+	'description' => 'TYPO3 extension for customizing a TYPO3 installation to meet the specific requirements of a customer\'s website.',
+	'category' => 'distribution',
+	'author' => 'Christian Pschorr',
+	'author_email' => 'pschorr.christian@gmail.com',
+	'author_company' => '',
+	'state' => 'beta',
+	'uploadfolder' => 0,
+	'clearCacheOnLoad' => 0,
+	'version' => '2.0.0',
+	'constraints' => [
+		'depends' => [
+			'typo3' => '12.0.0-12.4.99',
+		],
+		'conflicts' => [],
+		'suggests' => [],
+	],
+];
