a7030d44bff99491eac2be36e3a8ed1e2fd508df
Christian Pschorr <pschorr.christian@gmail.com>
2024-05-21 21:47:47 +0200

    Übernahme Overrides/tt_address aus XNA Extension

---
+++ Configuration/TCA/Overrides/tt_address.php
@@ -0,0 +1,6 @@
+<?php
+
+(function() {
+	$GLOBALS['TCA']['tt_address']['ctrl']['label_alt'] = 'last_name, first_name, company';
+	$GLOBALS['TCA']['tt_address']['ctrl']['label_alt_force'] = true;
+})();
