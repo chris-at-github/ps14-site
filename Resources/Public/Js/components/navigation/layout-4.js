61ea429873d452c46ae0bc247e9b0095b7e3b297
Christian Pschorr <pschorr.christian@gmail.com>
2024-06-12 00:27:11 +0200

    Umbau der Icons in der mobilen Navigation von SVG Sprite in Iconfont

--- Resources/Public/Js/components/navigation/layout-4.js
+++ Resources/Public/Js/components/navigation/layout-4.js
@@ -56,8 +56,16 @@
 
 			createBackLink: function(item, parent) {
 				let backlink = document.createElement('li'),
-					text = document.createElement('button'),
-					svg = xna.createSvgUseElement('#sprite-arrow-left', {'viewBox': '0 0 14 11'});
+					text = document.createElement('button');
+				// const svgNs = "http://www.w3.org/2000/svg";
+				//
+				// const svg = document.createElementNS(svgNs, 'svg');
+				// 	svg.setAttribute("height", "200");
+				// 	svg.setAttribute('viewBox', '0 0 14 11');
+				//
+				// const svgPath = document.createElementNS(svgNs, 'path');
+				// 	svgPath.setAttribute('d', 'M2633.84,2411.6L2630.8,2408.46C2630.71,2408.37 2630.66,2408.24 2630.66,2408.11C2630.66,2407.97 2630.72,2407.85 2630.81,2407.75C2630.97,2407.6 2631.16,2407.42 2631.32,2407.27C2631.41,2407.18 2631.54,2407.13 2631.67,2407.13C2631.8,2407.13 2631.93,2407.18 2632.02,2407.28C2632.99,2408.28 2635.71,2411.1 2636.68,2412.1C2636.74,2412.16 2636.78,2412.24 2636.8,2412.32C2636.82,2412.37 2636.82,2412.41 2636.82,2412.46C2636.83,2412.6 2636.78,2412.72 2636.68,2412.82C2635.72,2413.82 2632.99,2416.64 2632.02,2417.64C2631.93,2417.74 2631.8,2417.79 2631.67,2417.8C2631.54,2417.8 2631.41,2417.75 2631.32,2417.66C2631.16,2417.5 2630.97,2417.32 2630.81,2417.17C2630.72,2417.08 2630.66,2416.95 2630.66,2416.82C2630.66,2416.69 2630.71,2416.56 2630.8,2416.46L2633.85,2413.3L2623.44,2413.3C2623.17,2413.3 2622.94,2413.08 2622.94,2412.8C2622.94,2412.58 2622.94,2412.33 2622.94,2412.1C2622.94,2411.83 2623.17,2411.6 2623.44,2411.6L2633.84,2411.6Z');
+				// 	svgPath.setAttribute('transform', 'matrix(1,0,0,1,-2622.94,-2407.13) matrix(0.75047,0,0,2.79249,2433.3,1277.43) matrix(-1.3325,0,0,0.358103,3766.27,-457.451)');
 
 				backlink.classList.add('navigation-item--backlink');
 				backlink.addEventListener('click', function() {
@@ -65,7 +73,8 @@
 				});
 
 				text.innerText = xna.l10n.navigation.prev;
-				text.appendChild(svg);
+				// svg.appendChild(svgPath);
+				// text.appendChild(svg);
 
 				backlink.appendChild(text);
 
@@ -74,17 +83,17 @@
 
 			createParentLink: function(item, parent) {
 				let parentlink = document.createElement('li'),
-					link = item.cloneNode(true),
-					svg = xna.createSvgUseElement('#sprite-overview', {'viewBox': '0 0 11 12'});
+					link = item.cloneNode(true);
+					// svg = xna.createSvgUseElement('#sprite-overview', {'viewBox': '0 0 11 12'})
 
 				parentlink.classList.add('navigation-item--parentlink');
 
-				link.removeChild(link.querySelector('svg'));
+				// link.removeChild(link.querySelector('svg'));
 
-				// nur wenn das Elternelemet ein Link ist
-				if(link.tagName.toLocaleLowerCase() === 'a') {
-					link.appendChild(svg);
-				}
+				// // nur wenn das Elternelemet ein Link ist
+				// if(link.tagName.toLocaleLowerCase() === 'a') {
+				// 	link.appendChild(svg);
+				// }
 
 				parentlink.appendChild(link);
 
@@ -112,10 +121,10 @@
 				let active = false;
 				let timeout = false;
 
-				if(listItem.hasAttribute('aria-expanded') === true) {
-					let svg = xna.createSvgUseElement('#sprite-chevron-right', {'viewBox': '0 0 6 12'});
-					item.appendChild(svg);
-				}
+				// if(listItem.hasAttribute('aria-expanded') === true) {
+				// 	let svg = xna.createSvgUseElement('#sprite-chevron-right', {'viewBox': '0 0 6 12'});
+				// 	item.appendChild(svg);
+				// }
 
 				item.addEventListener('click', function(event) {
 					navigation.itemExpand(item, listItem);
